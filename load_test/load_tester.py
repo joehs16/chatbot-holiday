19c1226aae9d4c44e699bc0f11178d429f1de527
Jaryl <jarylngan95@gmail.com>
2021-04-30 12:35:27 +0800

    load test results

--- load_test/load_tester.py
+++ load_test/load_tester.py
@@ -6,5 +6,5 @@ class QuickstartUser(HttpUser):
     def loading_page(self):
         self.client.get("loadhomepage")
     def sending_inputtext(self):
-        self.client.post("inputtext", json={"book_hotel":"i would like to book a hotel"
+        self.client.post("inputtext", json={"book_hotel":"i would like to book a hotel",
                                             "book_car": "I would like to book a car"})
